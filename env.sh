5d33bc24505761123e017f1e2de59dba2ab06e3d
David Schere <dave.avantgarde@gmail.com>
2024-10-30 09:05:37 -0400

    untracked files not in repo

--- env.sh
+++ env.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-export GC_BASE_DIR="/home/david/proj/GuitarComposer"
+export GC_BASE_DIR=`pwd`
 export GC_CODE_DIR="$GC_BASE_DIR/src"
 export GC_DATA_DIR="$GC_BASE_DIR/data"
 export GC_BIN_DIR="$GC_BASE_DIR/bin"
