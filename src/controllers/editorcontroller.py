596cc626606ff775b7160b3ab507454f7670c887
David Schere <dave.avantgarde@gmail.com>
2024-11-19 19:58:13 -0500

    integrate toolbar with keystrokes (#8)

--- src/controllers/editorcontroller.py
+++ src/controllers/editorcontroller.py
@@ -12,7 +12,7 @@ from view.config import EditorKeyMap
 from view.events import Signals, EditorEvent
 from models.track import Track, StaffEvent, TabCursor
 from view.editor.trackEditor import TrackEditor
-
+from util.keyprocessor import KeyProcessor
 
 class Cursor:
     def __init__(self):
@@ -30,35 +30,7 @@ class EditorController:
     def _commit_fret_notes(self):
         pass
 
-    def _fret_ctl(self, key : int):
-        te = self.track_editor
-        tmodel = self.track_model
-        is_fret_value = False
-        if tmodel and te:
-            tc : TabCursor = tmodel.getTabCursor()
-            if key >= Qt.Key.Key_0 and key <= Qt.Key.Key_9: 
-                if tc.fret[tc.string] == 1:
-                    tc.fret[tc.string] = 10 + key - Qt.Key.Key_0
-                elif tc.fret[tc.string] == 2:
-                    tc.fret[tc.string] = 20 + key - Qt.Key.Key_0
-                    if tc.fret[tc.string] > 24:
-                        tc.fret[tc.string] = 24
-                else:
-                    tc.fret[tc.string] = key - Qt.Key.Key_0
-                is_fret_value = True
-            elif key == Qt.Key.Key_Space or Qt.Key.Key_Delete:
-                tc.fret[tc.string] = -1
-                is_fret_value = True
-            elif key == Qt.Key.Key_Enter:
-                # update staff and create track event
-                self._commit_fret_notes()
-
-            te.setFretValue(tc.presentation_col, tc.string, tc.fret[tc.string])
-            
-        return is_fret_value
-
-
-    def _up_key(self, tmodel: Track | None):
+    def _updown_key(self, tmodel: Track | None, inc: int):
         if tmodel and self.track_editor:
             te : TrackEditor = self.track_editor 
             tc = tmodel.getTabCursor()
@@ -67,7 +39,7 @@ class EditorController:
             #  ...
 
             # change the string we are pointing to
-            tc.string = (tc.string - 1) % nstring
+            tc.string = (tc.string - inc) % nstring
 
             # move the rectangle select box on the tableture
             te.setSelectRegion(tc.presentation_col, tc.string)
@@ -75,22 +47,6 @@ class EditorController:
             # clear the fret value
             #tc.fret = -1
 
-    def _down_key(self, tmodel: Track | None):
-        if tmodel and self.track_editor:
-            te : TrackEditor = self.track_editor 
-            tc = tmodel.getTabCursor()
-            nstring = len(tmodel.tuning)
-            # if fret value, add a note for this chord
-            #  ...
-
-            # change the string we are pointing to
-            tc.string = (tc.string + 1) % nstring
-
-            # move the rectangle select box on the tableture
-            te.setSelectRegion(tc.presentation_col, tc.string)
-
-            # clear the fret value
-            #tc.fret = None
 
     def update(self, tmodel: Track | None, editor: TrackEditor | None):
         if tmodel and editor:
@@ -131,16 +87,18 @@ class EditorController:
         key = evt.key
         # Check for arrow keys
         if key == Qt.Key.Key_Up:
-            self._up_key(self.track_model)
+            self._updown_key(self.track_model, 1)
         elif key == Qt.Key.Key_Down:
-            self._down_key(self.track_model)
+            self._updown_key(self.track_model, -1)
         elif key == Qt.Key.Key_Left:
             pass
         elif key == Qt.Key.Key_Right:
             pass
         else:
-            self._fret_ctl(key)
-
+            tc = self.track_model.getTabCursor() # type: ignore
+            self.key_proc.proc(key, tc) 
+            self.track_editor.setFretValue(tc.presentation_col, tc.string, tc.fret[tc.string])
+            self.track_editor.setToolbar(tc)
         
 
     dispatch = {
@@ -163,5 +121,6 @@ class EditorController:
         self.track_model = None
         self.track_editor = None
         # track model -> cursor
+        self.key_proc = KeyProcessor()
 
         Signals.editor_event.connect(self.editor_event)
