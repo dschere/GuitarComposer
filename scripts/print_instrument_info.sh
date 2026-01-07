2af77dff3b10a5edd75bb4232986f5bb4dcdf02a
David Schere <dave.avantgarde@gmail.com>
2026-01-06 21:45:49 -0500

    Tech debt/instrument info without fluidsynth (#111)
    * update, removed fluidsynth dependancy
    
    * removed unneeded files

--- scripts/print_instrument_info.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-export SFONT_NAME="$GC_DATA_DIR/sf/$1"
-export OUTFILE_DIR="$GC_DATA_DIR/sf_info"
-export SF_CMD_FILE="$GC_BASE_DIR/scripts/print_instruments.fs"
-
-
-#echo "$GC_BIN_DIR/fluidsynth -if $SF_CMD_FILE $SFONT_NAME > $OUTFILE"
-$GC_BIN_DIR/fluidsynth -if $SF_CMD_FILE $SFONT_NAME 
--- scripts/print_instruments.fs
+++
@@ -1,2 +0,0 @@
-inst 1
-quit
