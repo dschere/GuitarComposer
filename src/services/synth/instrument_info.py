385f7d3216ab16f95f6aea0af511fb004dd85c04
David Schere <dave.avantgarde@gmail.com>
2026-01-10 09:05:49 -0500

    Group info (#112)
    * add instrument group info
    
    * add untracked file

--- src/services/synth/instrument_info.py
+++ src/services/synth/instrument_info.py
@@ -21,10 +21,40 @@ class instrument_info:
     def find(self, name):
         return self.prefered.get(name, self.default_data.get(name))
     
+
+    def classify(self, gset, spec: instrument_spec):
+        group_name = "other"
+        for g in self.group_spec['groups']:
+            exclude_list = g.get('exclude_string',[])
+            match_list = g.get('match_string',[])
+
+            excluded = False
+            for token in exclude_list:
+                if token in spec.name:
+                    excluded = True
+                    break 
+
+            if not excluded:
+                for token in match_list:
+                    if token in spec.name:
+                        group_name = g['name']
+                        iset = gset.get(group_name,set())
+                        iset.add(spec.name)
+                        gset[group_name] = iset 
+                        return
+                
+        gset[group_name].add(spec.name)
+        
+
     def setup(self):
         sf_info_file = os.environ['GC_DATA_DIR']+"/sf_info/instruments.json"
         if not os.access(sf_info_file,os.F_OK):
-            raise RuntimeError("instrument info file missing is gcsyth running?")      
+            raise RuntimeError("instrument info file missing is gcsyth running?")
+        
+        gset = {
+            'other': set()
+        }
+
         sf_info_list = json.loads(open(sf_info_file).read())
         for sf_info in sf_info_list:
             sf_filename = sf_info['sf_filename']
@@ -42,8 +72,19 @@ class instrument_info:
 
                 table[spec.name] = spec
 
+                # find group classification
+                self.classify(gset, spec)
+
             self.instruments.append(sf_info)
 
+        for gname, iset in gset.items():
+            instr_list = list(iset)
+            instr_list.sort() 
+            self.groups[gname] = instr_list
+
+
+    def instrumentGroups(self):
+        return self.groups
             
     def __init__(self):
         sf_dir = os.environ['GC_DATA_DIR']+"/sf"
@@ -55,3 +96,7 @@ class instrument_info:
         self.prefered = {}
         # all others
         self.default_data = {}
+        self.groups = {}
+
+        ins_group_file = os.environ['GC_DATA_DIR']+"/instruments/groupings.json"
+        self.group_spec = json.loads(open(ins_group_file).read())
