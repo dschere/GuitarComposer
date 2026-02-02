7c956d1a9135f45a11120006b4928a57724776fb
David Schere <dave.avantgarde@gmail.com>
2026-02-01 19:24:36 -0500

    new file

---
+++ src/cmodules/gcsynth/gcsynth_fgraph_bindings.h
@@ -0,0 +1,10 @@
+#ifndef __GCSYNTH_FGRAPH_BINDINGS_H__
+#define __GCSYNTH_FGRAPH_BINDINGS_H__
+
+#include <Python.h>
+
+PyObject* gcsynth_clear_filter_graph(PyObject *self, PyObject *args);
+PyObject* gcsynth_config_filter_graph(PyObject *self, PyObject *args);
+
+
+#endif
